fffaf46844c58f1e74b57410beb890af457a4406
Soner Kochan <78814531+sonerkochan@users.noreply.github.com>
2026-04-07 22:07:24 +0300

    feat: Buttons leading to organizer/supplier dashboards in client dashboard

---
+++ EventHub/Areas/Supplier/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.Areas.Supplier.Controllers
+{
+    [Area("Supplier")]
+    [Route("/Supplier/[controller]/[Action]/{id?}")]
+    [Authorize(Roles = "Organizer,Admin")]
+    public class BaseController : Controller { }
+}
