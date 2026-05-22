e86ad01e15c0195585ec618b4355c6a9bd25371f
Soner Kochan <78814531+sonerkochan@users.noreply.github.com>
2026-05-22 11:19:24 +0300

    Add AnalyticsLog model for logging analytics data

---
+++ EventHub.Infrastructure/Data/Models/AnalyticsLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EventHub.Infrastructure.Data.Models
+{
+    public class AnalyticsLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? IpAddress { get; set; }
+        public string? Url { get; set; }
+        public string? Referrer { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Country { get; set; } = string.Empty;
+    }
+}
